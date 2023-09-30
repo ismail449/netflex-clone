1a4709bbd7fd7f1ce2784213db2a4405baea13a2
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-09-30 11:58:28 +0300

    added jsonwebtoken package

--- lib/db/hasura.ts
+++ lib/db/hasura.ts
@@ -1,13 +1,16 @@
 export async function queryHasuraGQL(
   operationsDoc: string,
   operationName: string,
-  variables: Record<string, any>
+  variables: Record<string, any>,
+  token: string
 ) {
+  console.log({ token });
   try {
     const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL ?? "", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_HASURA_JWT}`,
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
         query: operationsDoc,
@@ -20,3 +23,39 @@ export async function queryHasuraGQL(
     console.log(error);
   }
 }
+export async function isNewUser(token: string, didToken: string) {
+  const operationsDoc = `
+  query MyQuery {
+    users(where: {issuer: {_eq: ${didToken}}}) {      
+      email
+      id
+      issuer
+    }
+  }
+`;
+  const response = await queryHasuraGQL(operationsDoc, "MyQuery", {}, token);
+  console.log({ response });
+  return response?.data?.users?.length === 0;
+}
+function fetchMyQuery() {
+  const operationsDoc = `
+    query MyQuery {
+      users(where: {issuer: {_eq: ""}}) {
+        email
+        id
+        issuer
+      }
+    }
+  `;
+  return queryHasuraGQL(operationsDoc, "MyQuery", {}, "");
+}
+export async function startFetchMyQuery() {
+  const { errors, data } = await fetchMyQuery();
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+  // do something great with this precious data
+  console.log(data);
+}
+startFetchMyQuery();
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { magicAdmin } from "@/lib/magic";
+import jwt from "jsonwebtoken";
+import { isNewUser } from "@/lib/db/hasura";
 
 type Data = {
   response: any;
@@ -13,7 +15,21 @@ const login = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const didToken = req.headers.authorization?.substring(7) ?? "";
     const metaData = await magicAdmin.users.getMetadataByToken(didToken);
-    res.send({ response: metaData });
+    const token = jwt.sign(
+      {
+        ...metaData,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+        "https://hasura.io/jwt/claims": {
+          "x-hasura-allowed-roles": ["user", "admin"],
+          "x-hasura-default-role": "user",
+          "x-hasura-user-id": `${metaData.issuer}`,
+        },
+      },
+      "7MUCcF13iWfqmTLWeafaNQC5A2FgehNr"
+    );
+    const isNew = await isNewUser(token, didToken);
+    res.send({ response: { token, isNew } });
   } catch (error) {
     console.error("something went wrong logging in", error);
     res.status(500).send({ response: "not done" });
