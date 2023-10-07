4f6051ebe1b9f569200a42d027939c56754f403a
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-10-07 15:17:38 +0300

    refactored stats route

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -6,27 +6,32 @@ export default async function stats(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const token = req.cookies.token;
-  if (!token) {
-    res.status(403).send({ error: "Not Authorized" });
-    return;
-  }
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRET!);
-  if (typeof decodedToken === "string") {
-    res.status(400).send({ error: "error decoding JWT Token" });
-    return;
-  }
-  const userId = decodedToken.issuer;
-  if (req.method === "POST") {
-    try {
-      const { videoId, favourited, watched = true } = req.body;
-      if (!videoId || typeof favourited !== "number") {
-        res.status(400).send({ error: "video id or favourited are missing" });
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      res.status(403).send({ error: "Not Authorized" });
+      return;
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!);
+    if (typeof decodedToken === "string") {
+      res.status(400).send({ error: "error decoding JWT Token" });
+      return;
+    }
+    const userId = decodedToken.issuer;
+    const inputParams = req.method === "POST" ? req.body : req.query;
+    const { videoId } = inputParams;
+    if (!videoId) {
+      res.status(400).send({ error: "video id is missing" });
+      return;
+    }
+    const foundVideo = await findVideIdByUser(token, userId, videoId);
+    const isStatesFound = foundVideo.length > 0;
+    if (req.method === "POST") {
+      const { favourited, watched = true } = req.body;
+      if (typeof favourited !== "number") {
+        res.status(400).send({ error: "favourited is missing" });
         return;
       }
-
-      const foundVideo = await findVideIdByUser(token, userId, videoId);
-      const isStatesFound = foundVideo.length > 0;
       let responseStats;
       if (isStatesFound) {
         responseStats = await updateStats(token, {
@@ -44,31 +49,18 @@ export default async function stats(
         });
       }
       res.status(200).json({ data: responseStats });
-    } catch (error) {
-      res.status(500).send({ error });
-    }
-  } else if (req.method === "GET") {
-    try {
-      const videoId = req.query.videoId as string;
-      if (!videoId) {
-        res.status(400).send({ error: "video id is missing" });
-        return;
-      }
-      const foundVideo = await findVideIdByUser(token, userId, videoId);
-      const isStatesFound = foundVideo.length > 0;
+    } else if (req.method === "GET") {
       if (!isStatesFound) {
         res.status(404).send({ error: "video id not found" });
         return;
       }
 
       res.status(200).json({ foundVideo });
-    } catch (error) {
-      res.status(500).send({ error });
+    } else {
+      res.status(405).send({ response: "only POST method is allowed" });
+      return;
     }
-  } else {
-    console.log("GET");
-
-    res.status(405).send({ response: "only POST method is allowed" });
-    return;
+  } catch (error) {
+    res.status(500).send({ error });
   }
 }
